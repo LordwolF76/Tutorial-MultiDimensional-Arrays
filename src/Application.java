b56de647cb4108a1a87309626d7ebcf99af8889e
LordwolF76 <lordwolf76@gmail.com>
2016-07-31 23:17:26 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,40 @@
+/**
+ * Created by LordwolF on 7/31/2016.
+ */
+public class Application {
+    public static void main(String[] args) {
+
+        /* one dimensional array */
+        int[] values = {3, 5, 2343};
+
+        /* one dimension due to only one number needed in [] */
+        System.out.println(values[2]);
+
+        /* two dimensional array */
+        int[][] grid = {
+                {3, 5, 2343},
+                {2,4},
+                {1, 2, 3, 4}
+        };
+
+        System.out.println(grid[1][1]);
+        System.out.println(grid[2][0]);
+
+        String[][] texts = new String[2][3];
+
+        texts[0][1] = "hello there";
+
+        System.out.println(texts[0][1]);
+
+        for(int row=0; row<grid.length; row++) {
+            for(int col=0; col<grid[row].length; col++) {
+                System.out.print(grid[row][col] + "\t");
+            }
+
+            System.out.println();
+        }
+
+        String[][] words = new String[2][];
+        
+    }
+}
